f1bfb893c3c73cdedb57969f95f8172734b1d06a
miguelvalentino <143576441+miguelvalentino@users.noreply.github.com>
2024-05-27 13:20:50 +0700

    Add files via upload
    tes

---
+++ luassegitiga.py
@@ -0,0 +1,8 @@
+def luas_segi3(alas,tinggi):
+    luas = 0.5 * alas * tinggi
+    return luas
+
+alas = float(input("masukkan alas: "))
+tinggi = float(input("masukkan tinggi"))
+luas = luas_segi3(alas, tinggi)
+print("luas segitiga : %3f" %(luas))
