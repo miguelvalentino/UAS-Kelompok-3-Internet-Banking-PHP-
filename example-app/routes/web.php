81f91a2fb9b9bc629942ab8b088627d4c2242c5a
AdielAdornAdronAxleAxelAlex <nobody02983@gmail.com>
2024-06-01 10:40:24 +0700

    routes

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,3 +5,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/createaccount', function () {
+    return view('createaccount');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/deleteaccount', function () {
+    return view('deleteaccount');
+});
+
+Route::get('/deposit', function () {
+    return view('deposit');
+});
+
+Route::get('/withdraw', function () {
+    return view('withdraw');
+});
+
+Route::get('/changepassword', function () {
+    return view('changepassword');
+});
+
+Route::get('/biayaadmin', function () {
+    return view('biayaadmin');
+});
+
+Route::get('/deposito', function () {
+    return view('deposito');
+});
+
+Route::get('requestkartu', function () {
+    return view('requestkartu');
+});
